3fe9679c0018cb72bb6f0cd5284d22289f055275
Divyanshu Maithani <div.blackcat@gmail.com>
2017-04-13 15:23:03 +0530

    Throttling saveState to 1s

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,11 +1,12 @@
 import { createStore } from 'redux';
+import throttle from 'lodash/throttle';
 import { loadState, saveState } from './localStorage';
 
 import rootReducer from './reducers';
 
 const store = createStore(rootReducer, loadState());
-store.subscribe(() => {
+store.subscribe(throttle(() => {
   saveState(store.getState());
-});
+}, 1000));
 
 export default store;
